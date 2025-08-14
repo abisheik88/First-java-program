c401cff4e4249cda40885cfa34e3a3cf1d12d126
Abisheik <abishekvijayarangan@gmail.com>
2025-08-14 16:14:19 +0530

    New File

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
-        System.out.println("What is it?");
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello Who is back?");
 
         Scanner input=new Scanner(System.in);
         System.out.println(input.next());
-        System.out.println(input.nextInt());
     }
 }
