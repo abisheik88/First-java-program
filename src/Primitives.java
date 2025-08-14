0d5292d8a94bd1bfd93e1b9a2a6dd7cb1548b5bf
Abisheik <abishekvijayarangan@gmail.com>
2025-08-14 17:03:47 +0530

    Primitive Data types

--- src/Primitives.java
+++ src/Primitives.java
@@ -1,5 +1,15 @@
 public class Primitives {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        int rollno=64;
+        float marks=98.6f;
+        char letter='R';
+        double longInteger=4867890808.9867;
+        long largeInteger=34567890878877L;
+        boolean check=true;
+
+
+        System.out.println(marks);
     }
 }
