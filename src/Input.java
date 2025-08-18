2958856cc8d010f65ae5bd15e60e0583b744f466
Abisheik <abishekvijayarangan@gmail.com>
2025-08-18 21:34:05 +0530

    Changes

--- src/Input.java
+++ src/Input.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
+        Scanner integerInput=new Scanner(System.in);
         System.out.print("Please Enter your Roll No:");
-        int rollNo=input.nextInt();
+        int rollNo=integerInput.nextInt();
 
         System.out.println("Your Roll No is "+ rollNo);
 
         int a=2_34_000;
         System.out.println(a);
 
-        Scanner newInput=new Scanner(System.in);
-        String name=newInput.nextLine();
+        Scanner stringInput=new Scanner(System.in);
+        String name=stringInput.nextLine();
 
         System.out.println("My name is "+name);
 
