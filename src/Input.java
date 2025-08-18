e594dd24ba2b769e9fe50ecd30cb8ac7b080112d
Abisheik <abishekvijayarangan@gmail.com>
2025-08-18 20:56:51 +0530

    Inputs for Integer

---
+++ src/Input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int rollNo=input.nextInt();
+
+        System.out.println("My Roll No is "+ rollNo);
+    }
+}
