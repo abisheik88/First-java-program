93370833231b655550afa18aaff330e579f16ae7
Abisheik <abishekvijayarangan@gmail.com>
2025-08-18 21:33:19 +0530

    Changes

--- src/Input.java
+++ src/Input.java
@@ -7,5 +7,14 @@ public class Input {
         int rollNo=input.nextInt();
 
         System.out.println("Your Roll No is "+ rollNo);
+
+        int a=2_34_000;
+        System.out.println(a);
+
+        Scanner newInput=new Scanner(System.in);
+        String name=newInput.nextLine();
+
+        System.out.println("My name is "+name);
+
     }
 }
